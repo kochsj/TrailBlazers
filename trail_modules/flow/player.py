8cce714f05ab196d910facb8d359f61056f53c51
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2020-01-07 10:28:57 -0800

    wrote health status report method of Character class. wrote testing.

--- trail_modules/flow/player.py
+++ trail_modules/flow/player.py
@@ -10,6 +10,23 @@ class Character:
         self.alive = True
         self.food_stress_index = 100 #TODO: figure out this game logic
 
+    def return_health_status_report(self):
+        status_string = f'{self.name} is '
+        if 95 <= self.health <= 100:
+            status_string += "in perfect health."
+        if 80 <= self.health < 95:
+            status_string += "feeling great."
+        if 60 <= self.health < 80:
+            status_string += "feeling good."
+        if 35 <= self.health < 60:
+             status_string += "feeling poor."
+        if self.health < 35:
+            status_string += "in critical condition."
+        if self.sick == True:
+            status_string += " (SICK)"
+        
+        return status_string
+
 ###########################################################################################################
 def character_creation():
     """
