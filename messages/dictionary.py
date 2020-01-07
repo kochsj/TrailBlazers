b2bc106ea2835f6f613c5c7d54399fb67d45eeee
Amy Evans <amykarline@yahoo.com>
2020-01-07 11:02:19 -0800

    added all the fun facts

--- messages/dictionary.py
+++ messages/dictionary.py
@@ -3,23 +3,21 @@ def talk_to_people():
 
     talking_dictionary = {
     102: 'Hiya Traveler!! You\'ve reached the Kansas River crossing. This crossing is near, what will be known as Topeka, Kansas. Travelers cross the Kansas River to start their trek across Kansas and points out west. Be careful, the water can be deep in parts, and good luck on your journey',  
-
-    185: 'Welcome, to The Big Blue River Crossing! Did you know that this is the home of Alcove Spring and Waterfall, a popular area for pioneer wagons to ford the Big Blue River? This location was also the site of the first recorded death in the Donner Party - 70 year old Sarah H. Keyes, who died from consumption, I hope your travels have better results. ',
+    185: 'Welcome, to The Big Blue River Crossing! Did you know that this is the home of Alcove Spring and Waterfall, a popular area for pioneer wagons to ford the Big Blue River? This location is also the site of the first recorded death in the Donner Party - 70 year old Sarah H. Keyes, who died from consumption, I hope your travels have better results. ',
     304: "Howdy Traveler! Welcome to Fort Kearney,  a US Army post, located here on the south bank of the Platte River. We are the best supply outpost around these parts. Actually we're the only outpost around these part for the next 300 miles, so stock up if you need to, you have a long jouney ahead of you.", 
     554: 'Hi there stranger! This is Chimney Rock an important landmark on the Oregon Trail. It\'s a spectacular natural formation of solid rock and can be seen from miles around.  It also signals the end of the prairies as the trail is becoming more steep and rugged heading west towards the Rocky Mountains. Not much to do around here, but you won\'t ever forget this place.', 
     640: 'You\'ve reached Fort Laramie, a US Army post near the junction of the North Platte and Laramie Rivers.  It was founded as a fur-trading post in 1834, and named after a French trapper named Jacques Laramie, who is famous around these parts. Fort Laramie is an important stop for resting and getting supplies, so you should probably replace anything you\'ve lost before you head into the mountains.', 
     830: 'Well hello there! Independence Rock is an important landmark and resting place along the Oregon Trail. It\'s a large natural formation, almost 200 feet tall. It\'s made of soft stone -  many travelers carve their names or initials into it. ** Fun Fact ** It gets its name from the fact that, in order to stay on schedule, travelers try to reach it no later than July 4--Independence Day, hope you made it here in time, otherwise you could be in a world of hurt.',
-    932: 'South Pass',
-
+    932: 'Hi ya! Are you cutting through the South Pass?Well, that\'s a good thing since this is the only place along the Continental Divide that wagon trains can safely cross. It is the lowest point  between the Central and Southern Rocky Mountains, so get on your way - there\'s adventure waiting for you on the other side',
     989: 'Wow! You\'ve made it to Fort Bridger, and about halfway to your destination. A mountain man named Jim Bridger began this fort as a trading post in 1842, now it\'s one of the most important outfitting points along the Oregon Trail.  Stock up on your supplies while you\'re here, from what I hear conditions can get pretty treacherous going forward, and you\'ll want to be prepared.',
     1151: 'Welcome to the Green River Crossing! The Green River is a tributary to the Colorado River, flowing south from the Continental Divide along a twisted, rugged path, estimated to be more than 700 miles in length.  It\'s extremely dangerous to cross, but you must cross it if you want to contiue going west.  Good luck with that, youre going to need it.', 
-    1259: 'Soda Springs',
-    1359: 'Fort Hall', 
-    1543: 'Snake River Crossing', 
-    1648: 'Fort Boise', 
-    1808: 'Blue Mountains', 
-    1863: 'Fort Walla Walla',
-    2040: 'Oregon City'
+    1259: 'Aaaaah! Soda Springs is natual bubbling pools of carbonated water, caused by ancient volcanic activity. Local Indians, fur traders and trappers visit the pools of water for medicinal and bathing.  But don\'t drink too much of the water - it could make you sick.', 
+    1359: 'Fort Hall is an outpost on the banks of the Snake River.  Some people abandon their wagons here,and continue the rest of their way on foot with their animals. But that\'s silly, Dr. Marcus Whitman showed us all that you can lead a wagon train westward from here, so you should do that. This is a great place to buy supplies and rest up.',
+    1543: 'Welome to the Snake River Crossing. The Snake River gets its name from the way it twists and turns through this rugged country, sometimes through steep gorges. You\'ve safetly made it here, but crossing the Snake River can be very dangerous. Be careful my new friend.', 
+    1648: 'You\'ve made it to the last fort on the trail.  Fort Boise was built by the Hudson\'s Bay Company in 1834 as a fur-trading outpost. Now that the fur trade is dwindling, Fort Boise is becoming important supply post for travellers.',
+    1808: 'Grande Ronde in the Blue Mountains, The Grand Ronde (French for \'great ring\') is a river that runs parallel to the Blue Mountains.  The Grande Ronde valley a beautiful place to stop and rest, and enjoy the fact that your journey is almost over.', 
+    1863: 'Fort Walla Walla was established in 1818 as a fur-trading post where the Columbia and Walla Walla Rivers meet. This is the last fort you\'ll come to in the game. You\'re almost at the end, don\'t screw it up now!',
+    2040: 'Whoo Hoo!  You\'ve survived to the end of the trail!!!  Oregon City, has always been a natural place of commerce between Indians and whites, the town also uses the nearby river as a dependable source of power for mills for its economic development. This area is growing rapidly as an economic center of the territory. Go on an make your claim on some land, there\'s a lot of it out there to choose from.'
 
 }      
     return talking_dictionary
