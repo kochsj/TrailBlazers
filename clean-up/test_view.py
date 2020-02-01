4386099a2eaa869925734d070fc5fa648acd5277
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2020-02-01 15:05:31 -0800

    gitignore add .clean-up

--- clean-up/test_view.py
+++
@@ -1,8 +0,0 @@
-
-import arcade
-from gui_game.test_module import BaseView
-
-class TestView(arcade.View):
-    def on_show(self):
-        btn = arcade.TextButton(100,100,200,50,'Button')
-        self.button_list.append(btn)
